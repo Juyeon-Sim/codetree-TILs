85730a62066d2afd9ef31dacc4005427e068f7eb
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-24 07:20:52 +0900

    Title: 4번의 크기 비교, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,26 @@
+a=int(input())
+x=input().split()
+b=int(x[0])
+c=int(x[1])
+d=int(x[2])
+e=int(x[3])
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if a>c:
+    print(1)
+else:
+    print(0)
+
+if a>d:
+    print(1)
+else:
+    print(0)
+
+if a>e:
+    print(1)
+else:
+    print(0)
