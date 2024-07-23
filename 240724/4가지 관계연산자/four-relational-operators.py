fb825d439e10a086d326baf7a553eda284b449f8
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-24 07:18:53 +0900

    Title: 4가지 관계연산자, Time: 188ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,23 @@
+x=input().split()
+a=int(x[0])
+b=int(x[1])
+
+if a>=b:
+    print(1)
+else:
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if a<=b:
+    print(1)
+else:
+    print(0)
+
+if a<b:
+    print(1)
+else:
+    print(0)
