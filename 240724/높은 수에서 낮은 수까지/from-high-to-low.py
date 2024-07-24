c73e772ecf1e8d08477434d75cbe0d5c0d47841c
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-24 22:41:21 +0900

    Title: 높은 수에서 낮은 수까지, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,10 @@
+x=input().split()
+a=int(x[0])
+b=int(x[1])
+
+if a>b:
+    for i in range(a, b-1, -1):
+        print(i, end=' ')
+else:
+    for i in range(b, a-1, -1):
+        print(i, end=' ')
