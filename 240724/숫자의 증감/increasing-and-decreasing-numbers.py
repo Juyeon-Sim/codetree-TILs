c821030bcaa4b9db4398c1064dca8c11b79491ee
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-24 22:39:00 +0900

    Title: 숫자의 증감, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,9 @@
+x=input().split()
+c=str(x[0])
+n=int(x[1])
+if c=='A':
+    for i in range(1, n+1):
+        print(i, end=' ')
+elif c=='D':
+    for i in range(n, 0, -1):
+        print(i, end=' ')
