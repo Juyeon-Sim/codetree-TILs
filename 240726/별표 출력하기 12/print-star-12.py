8ca4838f21340f0326d070fe19ef240f7e5d59f7
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-26 07:01:00 +0900

    Title: 별표 출력하기 12, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,13 @@
+n=int(input())
+
+for i in range(n):
+    if i==0:
+        for _ in range(n):
+            print("*", end=' ')
+    else:
+        for j in range(n):
+            if j%2==1:
+                print("*", end=' ')
+            else:
+                print("  ")
+    print()
