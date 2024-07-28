b68cf229a1eb4ed1cc94a97620789011c58eed00
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-28 20:17:56 +0900

    Title: 십의 자리 숫자, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240728/십의 자리 숫자/number-of-tens-digit.py
+++ 240728/십의 자리 숫자/number-of-tens-digit.py
@@ -1,9 +1,12 @@
 arr=list(map(int, input().split()))
 n=len(arr)
+a=0
 
-for i in range(1, 10):
+for i in range(1,10):
     cnt=0
     for j in range(n):
-        if (arr[j]//10)==i:
+        if arr[j]==0:
+            break
+        if i==(arr[j]//10):
             cnt+=1
     print("%d - %d"%(i, cnt))
