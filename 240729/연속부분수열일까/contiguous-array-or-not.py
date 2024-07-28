8c2b3e38195f4bd65edf5d7135440c6691863324
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-29 01:46:29 +0900

    Title: 연속부분수열일까, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/연속부분수열일까/contiguous-array-or-not.py
+++ 240729/연속부분수열일까/contiguous-array-or-not.py
@@ -1,16 +1,17 @@
-arr_n1n2=list(map(int, input().split()))
-arr_A=list(map(int, input().split()))
-arr_B=list(map(int, input().split()))
-n1,n2=arr_n1n2[0],arr_n1n2[1]
+arr_n1n2 = list(map(int, input().split()))
+arr_A = list(map(int, input().split()))
+arr_B = list(map(int, input().split()))
+n1, n2 = arr_n1n2[0], arr_n1n2[1]
 
-a=arr_B[0]
-b=arr_A.index(a)
-arr=[]
+# arr_B가 arr_A의 부분 배열인지 확인
+is_subarray = False
 
-for i in range(b, b+n2):
-    arr.append(arr_A[i])
+for i in range(n1 - n2 + 1):
+    if arr_A[i:i + n2] == arr_B:
+        is_subarray = True
+        break
 
-if arr==arr_B:
+if is_subarray:
     print("Yes")
 else:
     print("No")
