bb8d1099539a1832523620e34cf4e48189576457
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-28 17:57:40 +0900

    Title: 짝수만 거꾸로 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240728/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
+++ 240728/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -3,10 +3,11 @@ arr=list(map(int, input().split()))
 arr2=[]
 a=0
 
-for i in arr:
-    if i%2==0:
-        arr2.append(i)
-        a+=0
+for i in range(n):
+    for j in arr:
+        if j%2==0:
+            arr2.append(i)
+            a+=1
 
 for i in range(a, -1, -1):
     print(arr2[i], end=' ')
