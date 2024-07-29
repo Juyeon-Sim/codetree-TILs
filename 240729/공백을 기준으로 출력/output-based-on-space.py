2bd2fea3809bd3df5cc9150f2c343d1ac6780d47
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-29 23:55:13 +0900

    Title: 공백을 기준으로 출력, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240729/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,13 @@
+str1=input()
+str2=input()
+
+for i in range(len(str1)):
+    if str1[i]==' ':
+        pass
+    else:
+        print(str1[i], end='')
+for i in range(len(str2)):
+    if str2[i]==' ':
+        pass
+    else:
+        print(str2[i], end='')
