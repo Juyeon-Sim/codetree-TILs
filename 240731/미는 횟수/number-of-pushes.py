6677b589fee790cc962295776ed0afeafc22a45d
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-31 05:51:32 +0900

    Title: 미는 횟수, Time: 150ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/미는 횟수/number-of-pushes.py
+++ 240731/미는 횟수/number-of-pushes.py
@@ -1,12 +1,15 @@
-A=str(input())
-B=str(input())
-cnt=0
+A = input()
+B = input()
 
-while True:
-    if A==B:
-        break
+# 문자열 회전 가능 여부 확인
+if len(A) != len(B):
+    print(-1)
+else:
+    # A+A에 B가 포함되는지 확인
+    concatenated = A + A
+    if B in concatenated:
+        # B가 포함되는 위치를 찾아 회전 횟수 계산
+        cnt = concatenated.index(B)
+        print(cnt)
     else:
-        A=A[-1]+A[:len(A)-1]
-        cnt+=1
-
-print(cnt)
+        print(-1)
