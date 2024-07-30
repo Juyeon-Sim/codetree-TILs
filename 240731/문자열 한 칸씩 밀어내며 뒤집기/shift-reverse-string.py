455e382694b542a01624ae85281047a0d6d01589
Juyeon-Sim <tlawndus1220@naver.com>
2024-07-31 04:12:52 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
+++ 240731/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -1,24 +1,19 @@
-arr=input().split()
-s=str(arr[0])
-q=int(arr[1])
-ss=list(s)
+arr = input().split()
+s = arr[0]
+q = int(arr[1])
+ss = list(s)
 
-for i in range(q):
-    a=int(input())
-    if a==1:
-        ss=ss[1:len(s)]+[ss[0]]
-        for j in ss:
-            print(j, end='')
-        print()
-        #0번을 마지막으로
-    elif a==2:
-        ss=[ss[len(s)-1]]+ss[:len(s)-1]
-        for j in ss:
-            print(j, end='')
-        print()
-        #마지막을 0번으로
+for _ in range(q):
+    a = int(input())
+    
+    if a == 1:
+        # 첫 번째 문자를 마지막으로 이동
+        ss = ss[1:] + [ss[0]]
+        print(''.join(ss))
+    elif a == 2:
+        # 마지막 문자를 첫 번째로 이동
+        ss = [ss[-1]] + ss[:-1]
+        print(''.join(ss))
     else:
-        for j in range(len(s)-1, -1, -1):
-            print(ss[j], end='')
-        print()
-        #반대로
+        # 문자열을 반전하여 출력
+        print(''.join(ss[::-1]))
